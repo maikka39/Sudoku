96f6641454782beac94dbbd3ba61007286e8259c
Maik de Kruif <maik@maik.dev>
2022-05-12 17:57:56 +0200

    Add jigsaw sudoku support

--- src/main/scala/sudoku/parsers/SudokuParser.scala
+++ src/main/scala/sudoku/parsers/SudokuParser.scala
@@ -1,7 +1,7 @@
 package sudoku.parsers
 
 import sudoku.models.Sudoku.SudokuField
-import sudoku.models.{RegularSudoku, Sudoku}
+import sudoku.models.{JigsawSudoku, Position, RegularSudoku, Sudoku}
 
 import scala.io.Source
 
@@ -11,12 +11,12 @@ object SudokuParser {
 
     val source = Source.fromFile(filePath)
     val inputData =
-      try source.getLines.mkString
+      try source.getLines.filter(!_.startsWith("#")).mkString
       finally source.close()
 
     puzzleType match {
       case "4x4" | "6x6" | "9x9" => RegularSudokuParser.parse(inputData)
-      case "jigsaw"              => ???
+      case "jigsaw"              => JigsawSudokuParser.parse(inputData)
       case "samurai"             => ???
     }
 
@@ -28,15 +28,48 @@ object RegularSudokuParser {
     val size = Math.sqrt(inputData.length).intValue
 
     val grid = inputData
+      .map(_.toInt - 48)
+      .map(n => SudokuField(if (n != 0) Some(n) else None, Seq(), isPermanent = n != 0))
       .sliding(size, size)
       .toSeq
       .transpose
-      .map(a =>
-        a
-          .map(_.toInt - 48)
-          .map(n => SudokuField(if (n != 0) Some(n) else None, Seq(), isPermanent = n != 0))
-      )
 
     RegularSudoku(grid)
+  }
+}
+
+object JigsawSudokuParser {
+  def parse(inputData: String): JigsawSudoku = {
+    val jigsawData = inputData.drop(10).split("=")
+
+    val grid = jigsawData
+      .map(_(0))
+      .map(_.toInt - 48)
+      .map(n => SudokuField(if (n != 0) Some(n) else None, Seq(), isPermanent = n != 0))
+      .sliding(9, 9)
+      .toSeq
+      .transpose
+
+    val groupNumbers = jigsawData
+      .map(_(2))
+      .map(_.toInt - 48)
+      .sliding(9, 9)
+      .toSeq
+      .transpose
+
+    val groupPositions = for {
+      (col, x)         <- groupNumbers.zipWithIndex
+      (groupNumber, y) <- col.zipWithIndex
+    } yield (groupNumber, Position(x, y))
+
+    val fieldGroups = groupPositions
+      .groupBy(_._1)
+      .view
+      .mapValues(positions => positions.map(_._2))
+      .toSeq
+      .sortBy(_._1)
+      .map(_._2)
+
+    JigsawSudoku(grid, fieldGroups)
   }
 }
--- src/test/scala/sudoku/parsers/SudokuParserSpec.scala
+++ src/test/scala/sudoku/parsers/SudokuParserSpec.scala
@@ -2,7 +2,7 @@ package sudoku.parsers
 
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.must.Matchers
-import sudoku.models.RegularSudoku
+import sudoku.models.{JigsawSudoku, Position, RegularSudoku}
 import sudoku.models.Sudoku.SudokuField
 
 class SudokuParserSpec extends AnyWordSpec with Matchers {
@@ -192,6 +192,211 @@ class SudokuParserSpec extends AnyWordSpec with Matchers {
     )
   )
 
+  lazy val jigsawSudoku: JigsawSudoku = JigsawSudoku(
+    List(
+      List(
+        SudokuField(None, List()),
+        SudokuField(Some(8), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(3), List(), isPermanent = true),
+        SudokuField(None, List())
+      ),
+      List(
+        SudokuField(Some(8), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(7), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(9), List(), isPermanent = true)
+      ),
+      List(
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(4), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List())
+      ),
+      List(
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(1), List(), isPermanent = true),
+        SudokuField(Some(3), List(), isPermanent = true),
+        SudokuField(Some(8), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(Some(6), List(), isPermanent = true),
+        SudokuField(None, List())
+      ),
+      List(
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(9), List(), isPermanent = true),
+        SudokuField(Some(6), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(Some(7), List(), isPermanent = true),
+        SudokuField(Some(5), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List())
+      ),
+      List(
+        SudokuField(None, List()),
+        SudokuField(Some(7), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(Some(9), List(), isPermanent = true),
+        SudokuField(Some(1), List(), isPermanent = true),
+        SudokuField(Some(6), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List())
+      ),
+      List(
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(8), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List())
+      ),
+      List(
+        SudokuField(Some(5), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(3), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(8), List(), isPermanent = true)
+      ),
+      List(
+        SudokuField(None, List()),
+        SudokuField(Some(5), List(), isPermanent = true),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(None, List()),
+        SudokuField(Some(2), List(), isPermanent = true),
+        SudokuField(None, List())
+      )
+    ),
+    List(
+      List(
+        Position(0, 0),
+        Position(1, 0),
+        Position(1, 1),
+        Position(1, 2),
+        Position(1, 3),
+        Position(1, 4),
+        Position(1, 5),
+        Position(1, 6),
+        Position(2, 0)
+      ),
+      List(
+        Position(2, 1),
+        Position(2, 2),
+        Position(2, 3),
+        Position(2, 4),
+        Position(3, 0),
+        Position(3, 1),
+        Position(3, 2),
+        Position(3, 3),
+        Position(3, 4)
+      ),
+      List(
+        Position(4, 0),
+        Position(4, 1),
+        Position(4, 2),
+        Position(5, 0),
+        Position(5, 1),
+        Position(5, 2),
+        Position(6, 0),
+        Position(7, 0),
+        Position(8, 0)
+      ),
+      List(
+        Position(0, 1),
+        Position(0, 2),
+        Position(0, 3),
+        Position(0, 4),
+        Position(0, 5),
+        Position(0, 6),
+        Position(0, 7),
+        Position(1, 7),
+        Position(2, 7)
+      ),
+      List(
+        Position(6, 1),
+        Position(7, 1),
+        Position(8, 1),
+        Position(8, 2),
+        Position(8, 3),
+        Position(8, 4),
+        Position(8, 5),
+        Position(8, 6),
+        Position(8, 7)
+      ),
+      List(
+        Position(2, 5),
+        Position(2, 6),
+        Position(3, 5),
+        Position(4, 3),
+        Position(4, 4),
+        Position(4, 5),
+        Position(5, 3),
+        Position(6, 2),
+        Position(6, 3)
+      ),
+      List(
+        Position(6, 8),
+        Position(7, 2),
+        Position(7, 3),
+        Position(7, 4),
+        Position(7, 5),
+        Position(7, 6),
+        Position(7, 7),
+        Position(7, 8),
+        Position(8, 8)
+      ),
+      List(
+        Position(5, 4),
+        Position(5, 5),
+        Position(5, 6),
+        Position(5, 7),
+        Position(5, 8),
+        Position(6, 4),
+        Position(6, 5),
+        Position(6, 6),
+        Position(6, 7)
+      ),
+      List(
+        Position(0, 8),
+        Position(1, 8),
+        Position(2, 8),
+        Position(3, 6),
+        Position(3, 7),
+        Position(3, 8),
+        Position(4, 6),
+        Position(4, 7),
+        Position(4, 8)
+      )
+    )
+  )
+
   "SudokuParser" should {
     "correctly read a 4x4 puzzle" in {
       val sudoku = SudokuParser.parse("./src/test/resources/puzzles/puzzle.4x4")
@@ -209,6 +414,12 @@ class SudokuParserSpec extends AnyWordSpec with Matchers {
       val sudoku = SudokuParser.parse("./src/test/resources/puzzles/puzzle.9x9")
 
       sudoku mustBe regularSudoku9x9
+    }
+
+    "correctly read a jigsaw puzzle" in {
+      val sudoku = SudokuParser.parse("./src/test/resources/puzzles/puzzle.jigsaw")
+
+      sudoku mustBe jigsawSudoku
     }
   }
 }
