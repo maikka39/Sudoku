a59fe2e4d42c8f5ce682e8e0b9fafc80e7cadc7e
Maik de Kruif <maik@maik.dev>
2022-05-31 23:20:38 +0200

    Only draw lines around active fields

--- src/main/scala/sudoku/view/printers/SudokuPrinter.scala
+++ src/main/scala/sudoku/view/printers/SudokuPrinter.scala
@@ -2,43 +2,77 @@ package sudoku.view.printers
 
 import sudoku.models.Sudoku
 import sudoku.view.display.Display
-import sudoku.view.display.Display.{Position, TextStyle}
+import sudoku.view.display.Display.{Color, Position, TextStyle, createColorPair}
 
 object SudokuPrinter {
+  private val permanentNumberColor = createColorPair(Color.WHITE, Color.BLACK)
+
   def print(startPosition: Position, sudoku: Sudoku): Unit = {
     Display.moveCursor(startPosition)
 
-    for (_ <- sudoku.grid.head) {
-      Display.print("+---")
-    }
-    Display.print("+")
+    drawHorizontalLine(sudoku.grid, 0)
 
     for ((column, columnIndex) <- sudoku.grid.zipWithIndex) {
       Display.moveCursor(Position(startPosition.x, startPosition.y + (columnIndex * 2) + 1))
-      Display.print("|")
-      for (field <- column) {
+
+      if (column.headOption.exists(_.isActive))
+        Display.print("|")
+      else
         Display.print(" ")
 
-        if (field.isPermanent)
+      for ((field, fieldIndex) <- column.zipWithIndex) {
+        Display.print(" ")
+
+        if (field.isPermanent) {
           Display.addTextStyles(TextStyle.BOLD)
+          Display.setColor(permanentNumberColor)
+        }
 
         Display.print(field.number.map(_.toString).getOrElse(" "))
 
-        if (field.isPermanent)
-          Display.setTextStyle(TextStyle.NORMAL)
+        Display.setTextStyle(TextStyle.NORMAL)
 
-        Display.print(" |")
+        Display.print(" ")
+
+        if (field.isActive || column.drop(fieldIndex + 1).headOption.exists(_.isActive))
+          Display.print("|")
+        else
+          Display.print(" ")
       }
 
       Display.moveCursor(Position(startPosition.x, startPosition.y + (columnIndex * 2) + 2))
-      for (_ <- sudoku.grid.head) {
-        Display.print("+---")
-      }
-      Display.print("+")
 
+      drawHorizontalLine(sudoku.grid, columnIndex)
     }
 
     Display.moveCursor(startPosition)
     Display.refresh()
+  }
+
+  private def drawHorizontalLine(grid: Sudoku.Grid, index: Int): Unit = {
+    for ((field, fieldIndex) <- grid.transpose.drop(index).head.zipWithIndex) {
+      if (
+        field.isActive || grid.transpose
+          .drop(index + 1)
+          .headOption
+          .flatMap(_.drop(fieldIndex).headOption)
+          .exists(_.isActive)
+      )
+        Display.print("+---")
+      else if (grid.transpose.drop(index).head.drop(fieldIndex - 1).headOption.exists(_.isActive))
+        Display.print("+   ")
+      else
+        Display.print("    ")
+    }
+    if (
+      grid.transpose
+        .drop(index)
+        .head
+        .last
+        .isActive || grid.transpose.drop(index + 1).headOption.map(_.last).exists(_.isActive)
+    )
+      Display.print("+")
+    else
+      Display.print(" ")
   }
 }
