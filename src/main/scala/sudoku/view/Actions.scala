c06e1c76b99f9d2981c04650991b8669d0daa923
Maik de Kruif <maik@maik.dev>
2022-06-01 13:29:05 +0200

    Make solve action work

--- src/main/scala/sudoku/view/Actions.scala
+++ src/main/scala/sudoku/view/Actions.scala
@@ -1,20 +1,26 @@
 package sudoku.view
 
-import sudoku.models.{Game, Action => GameAction}
+import sudoku.models.{SolveAction, Action => GameAction}
+import sudoku.solvers.BacktrackingSudokuSolver
 import sudoku.view.display.Display
 
 object Actions {
-  protected final case class Action(keybind: Char, label: String, onCall: Unit => Option[GameAction])
+  sealed case class Action private (keybind: Char, label: String, onCall: Unit => Option[GameAction])
 
   val actions: Seq[Action] = Seq(
     Action('q', "Quit", { _ => onQuit() }),
-    Action('s', "Solve puzzle", { _ => ??? }),
-    Action('o', "Open puzzle", { _ => ??? })
+    Action('s', "Solve puzzle", { _ => onSolve() }),
+    Action('o', "Open puzzle", { _ => ??? }),
+    Action('r', "Redraw", { _ => None })
   )
 
   private def onQuit(): Option[GameAction] = {
     Display.quit()
     System.exit(0)
     None
+  }
+
+  private def onSolve(): Option[GameAction] = {
+    Some(SolveAction(BacktrackingSudokuSolver))
   }
 }
